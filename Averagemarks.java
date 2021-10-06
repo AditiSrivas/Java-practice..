437e2ce8f35bc04d594d22b7e498cacbb43263a8
Aditi Srivastava <64702777+AditiSrivas@users.noreply.github.com>
2021-10-06 21:05:27 +0530

    Add files via upload

---
+++ Averagemarks.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Averagemarks {
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+        String a= sc.next();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        System.out.println();
+        System.out.println(a);
+        int av=(b+c+d)/3;
+        System.out.println(av);
+		
+	}
+
+}
