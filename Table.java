d507fab375a8ad8bfcdb396733d72c8e5a269528
Aditi Srivastava <64702777+AditiSrivas@users.noreply.github.com>
2021-10-07 13:05:32 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,24 @@
+/*Write a program to print multiplication table of n*/
+import java.util.*;
+public class Table {
+	
+	public static void main(String[] args) {
+		
+     Scanner sc=new Scanner(System.in);
+        float b=sc.nextInt();
+        char g=sc.next().charAt(0);
+        float hra=b*0.2f;
+        float da=b*0.5f;
+        int allow=0;
+        if(g=='A')
+            allow=1700;
+        else if(g=='B')
+            allow=1500;
+        else 
+            allow=1300;
+        float pf=b*0.11f;
+        float total=b+hra+da+allow-pf;
+        total=total+0.5f;
+        System.out.println((int)total);
+	}
+}
